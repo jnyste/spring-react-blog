7b2d0b0e2f4f5f19bfbce39583f453979724997f
Jan-Sebastian Nystedt <jan-sebastian.nystedt@cs.tamk.fi>
2019-03-15 22:25:52 +0200

    Create CommentRepository class

---
+++ src/main/java/me/jsbn/blogapplication/repository/CommentRepository.java
@@ -0,0 +1,12 @@
+package me.jsbn.blogapplication.repository;
+
+import me.jsbn.blogapplication.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPostId(Long postId);
+}
