5f0496362d536f2f58808a1f5996228b0bbf8dec
Jan-Sebastian Nystedt <jan-sebastian.nystedt@cs.tamk.fi>
2019-03-21 00:08:17 +0200

    Add PostLike class

---
+++ src/main/java/me/jsbn/blogapplication/model/PostLike.java
@@ -0,0 +1,39 @@
+package me.jsbn.blogapplication.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "post_likes")
+public class PostLike {
+
+    @Id
+    @GeneratedValue(generator = "postlike_generator")
+    @SequenceGenerator(name = "postlike_generator", sequenceName = "postlike_sequence")
+
+    private Long id;
+    private Long post;
+
+    public PostLike() {
+
+    }
+
+    public PostLike(Long postId) {
+        this.post = postId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getPost() {
+        return post;
+    }
+
+    public void setPost(Long post) {
+        this.post = post;
+    }
+}
