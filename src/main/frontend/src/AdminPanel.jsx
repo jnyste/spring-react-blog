7282e9be84c777a00831b80a50e919589cab9dfe
Jan-Sebastian Nystedt <jan-sebastian.nystedt@cs.tamk.fi>
2019-03-20 19:54:38 +0200

    Add AdminPanel component

---
+++ src/main/frontend/src/AdminPanel.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './css/App.css';
+
+class AdminPanel extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default AdminPanel;
