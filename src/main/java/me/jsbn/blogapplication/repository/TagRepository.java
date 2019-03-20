325246d384d17101f5fa7a3a62e1512f5708c8fe
Jan-Sebastian Nystedt <jan-sebastian.nystedt@cs.tamk.fi>
2019-03-20 20:08:07 +0200

    Add TagRepository class

---
+++ src/main/java/me/jsbn/blogapplication/repository/TagRepository.java
@@ -0,0 +1,11 @@
+package me.jsbn.blogapplication.repository;
+
+import me.jsbn.blogapplication.model.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Long> {
+    List<Tag> findByContent(String content);
+}
